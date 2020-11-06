46ce3c6efde822b4d5191d7e04604d74ec28ccac
Shawn Benny <sbenn9210@gmail.com>
2020-11-06 08:32:51 -0600

    remove comments

--- app.py
+++ app.py
@@ -22,3 +22,5 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# TODO: {print __name__}
--- vars.py
+++ vars.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-print(__name__)
-
 @app.route('/<name>')
 def greet(name):
     return f'Hello {name}'
