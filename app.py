2e1445a4f04e7e80d3afc7d6722bc6531b984fd0
Shawn Benny <sbenn9210@gmail.com>
2020-11-06 08:29:27 -0600

    Add templates and base template

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('base.html', list_of_names=['chris', "Shawn", "Jackson"])
 
 @app.route('/shawn')
 def hello_shawn():
@@ -14,6 +14,10 @@ def hello_shawn():
 @app.route('/<name>')
 def greet(name):
     return f'Hello {name}'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
--- vars.py
+++ vars.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+print(__name__)
+
 @app.route('/<name>')
 def greet(name):
     return f'Hello {name}'
