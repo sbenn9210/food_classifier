f49af1533fbd2c989481fce44c6de4665e45f661
Shawn Benny <sbenn9210@gmail.com>
2020-11-07 20:27:45 -0600

    Add sqlite database with SQLAlchemy

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 from forms import Todo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'password'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+db = SQLAlchemy(app)
+
+class TodoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(240))
+
+    def __str__(self):
+        return f'{self.content}, {self.id}'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     # return render_template('base.html', list_of_names=['chris', "Shawn", "Jackson"])
     request_method = request.method
+    todo = TodoModel.query.all()
     if request.method == 'POST':
         first_name = request.form['first_name']
         return redirect(url_for("name", first_name=first_name))
-    return render_template('hello.html', request_method=request_method)
+    return render_template('hello.html', request_method=request_method, todo=todo)
 
 
 @app.route('/name/<string:first_name>')
@@ -24,6 +34,9 @@ def name(first_name):
 def todo():
     todo_form = Todo()
     if todo_form.validate_on_submit():
+        todo = TodoModel(content=todo_form.content.data)
+        db.session.add(todo)
+        db.session.commit()
         print(todo_form.content.data)
         return redirect('/')
     return render_template('todo.html', form=todo_form)
